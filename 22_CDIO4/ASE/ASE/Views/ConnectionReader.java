5269ffc1f645a6092c17244ca4ea5d37f59980f3
Mads <mads@stege.eu>
2017-06-02 14:00:11 +0200

    Implementation of Connection reader w. error handling.
    Includes a .txt file for the Weight Table.

--- 22_CDIO4/ASE/ASE/Views/ConnectionReader.java
+++ 22_CDIO4/ASE/ASE/Views/ConnectionReader.java
@@ -1,5 +1,36 @@
 package ASE.Views;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class ConnectionReader {
 
+	String WeightIP, WeightPort;
+
+	public void WeightReader() throws FileNotFoundException {
+
+		// Attempt to retrieve information from the WeightTable.txt file.
+		try {
+			// Creation of the scanner
+			Scanner WeightScanner = new Scanner(new FileInputStream("WeightTable.txt"));
+
+			// Retrieve information
+			while (WeightScanner.hasNextLine()) {
+				String newWeightLine = WeightScanner.nextLine().trim();
+				if (newWeightLine.equals("XXX")) {
+					WeightIP = newWeightLine;
+				} else if (newWeightLine.equals("YYY")) {
+					WeightPort = newWeightLine;
+				}
+				// Print information for confirmation
+				System.out.println("WeightIP " + "WeightPort");
+			}
+
+			WeightScanner.close();
+
+		} catch (Exception e) {
+			System.out.println("Error occured: " + e);
+		}
+	}
 }
