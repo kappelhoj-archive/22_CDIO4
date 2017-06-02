d4fe890d08823e3d7be344a89d49dc9abcd271d6
Arvid Langsø <arvid.k.langsoe@live.dk>
2017-06-02 14:08:12 +0200

    Added a few methods to the WeightController

--- 22_CDIO4/ASE/ASE/Controllers/WeightController.java
+++ 22_CDIO4/ASE/ASE/Controllers/WeightController.java
@@ -1,5 +1,41 @@
 package ASE.Controllers;
 
-public class WeightController {
+import java.io.IOException;
+import java.net.Socket;
+import java.net.UnknownHostException;
 
+public class WeightController implements Runnable {
+	MeasurementController measurementAdder;
+	Socket weightConnection;
+
+	WeightController(MeasurementController measurementAdder, String ip, int port)
+			throws UnknownHostException, IOException {
+		this.measurementAdder = measurementAdder;
+		weightConnection = new Socket(ip, port);
+	}
+
+	@Override
+	public void run() {
+		while(true){
+			 startMeasureMent();
+		}
+	}
+	
+	public void startMeasureMent(){
+		
+	}
+	
+	public void login(){
+		
+	}
+	
+	
+	
+	public void handleReturnMessage(String messageSent, String answerReceived){
+		switch(answerReceived){
+			default:
+				break;
+		}
+	}
+	
 }
