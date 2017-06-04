d95e685dbca3034d8b09e474415ae5d20184c5d2
Mads <mads@stege.eu>
2017-06-04 15:55:37 +0200

    Not finished, still needs work. Getting reworked on monday (05/06-17).

--- 22_CDIO4/ASE/ASE/Views/ConnectionManager.java
+++ 22_CDIO4/ASE/ASE/Views/ConnectionManager.java
@@ -6,28 +6,46 @@ import java.net.Socket;
 import java.net.UnknownHostException;
 import java.io.IOException;
 
-
 public class ConnectionManager implements Runnable {
 
-	ConnectionReader connectionReader;
+	ConnectionReader connectionReader = new ConnectionReader();
 	WeightController weightController;
 	MeasurementController measurementController;
 	Socket weightSocket;
-	
+
 	@Override
 	public void run() {
 		try {
 			weightSocket = new Socket(connectionReader.WeightIP, connectionReader.getPortInt());
 			System.out.println("Connection established.");
 		} catch (UnknownHostException e) {
-			System.out.println("Error occured: Host unknown "+ e);
+			System.out.println("Error occured: Host unknown " + e);
 		} catch (IOException e) {
-			System.out.println("Error occured: Port number unknown "+ e);
-		}	
+			System.out.println("Error occured: Port number unknown " + e);
+		}
 
 	}
-	public static void main(String args[]) {
-        (new Thread(new WeightController(MeasurementController.measurementAdder, ip, port))).start();
-    }
+
+	public void threadStarter() {
+		for (int i = 0; i < connectionReader.getIPArray().length; i++);
+			try {
+				weightSocket = new Socket(connectionReader.getIPString(), connectionReader.getPortInt());
+			} catch (IOException e) {
+				System.out.println("Connection failed: " +e);
+			}
+			
+			weightController = new WeightController(measurementController, weightSocket);
+			
+			(new Thread(new weightThread())).start();
+			(new weightThread()).start();
+			
+			
+			
+		// For hver IP/Port detekteret. Check
+		// Lav nyt socket med ip/port Check
+		// Lav ny weightController til hvert socket.
+		// Start tråd
+		// tråd.start;
+	}
 
 }
