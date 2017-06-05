3034d80171066bf047e273e9f5a729b0dc2520d5
Peter <peterxp@hotmail.fr>
2017-06-05 10:17:26 +0200

    BugFixes

--- 22_CDIO4/src/dataAccessObjects/MyProduktBatchKompDAO.java
+++ 22_CDIO4/src/dataAccessObjects/MyProduktBatchKompDAO.java
@@ -10,25 +10,25 @@ import dataTransferObjects.ProduktBatchKompDTO;
 import exceptions.DALException;
 
 public class MyProduktBatchKompDAO implements ProduktBatchKompDAO {
-	
+
 	class DoubleInteger {
 		int x;
 		int y;
-		
+
 		int getX(){return x;}
 		int getY(){return y;}
-		
+
 		public DoubleInteger(int x, int y){this.x = x; this.y = y;}
 	}
-	
+
 	static Hashtable<DoubleInteger, ProduktBatchKompDTO> productBatchCompList = new Hashtable<DoubleInteger, ProduktBatchKompDTO>();
 
 	@Override
 	public ProduktBatchKompDTO getProduktBatchKomp(int pbId, int rbId) throws DALException {
-		
+
 		if(productBatchCompList.get(new DoubleInteger(pbId, rbId)) != null)
 			return productBatchCompList.get(new DoubleInteger(pbId, rbId)).copy();
-	
+
 		else
 			throw new DALException("Unknown Product Batch Comp ID: " + rbId);
 	}
@@ -38,7 +38,21 @@ public class MyProduktBatchKompDAO implements ProduktBatchKompDAO {
 		List<ProduktBatchKompDTO> productbsc = new ArrayList<ProduktBatchKompDTO>();
 
 		Set<DoubleInteger> keys = productBatchCompList.keySet();
-		
+
+		for(DoubleInteger key : keys){
+			if(productBatchCompList.get(key).getPbId() == pbId)
+				productbsc.add(productBatchCompList.get(key).copy());
+		}
+
+		return productbsc;
+	}
+
+	@Override
+	public List<ProduktBatchKompDTO> getProduktBatchKompList() throws DALException {
+		List<ProduktBatchKompDTO> productbsc = new ArrayList<ProduktBatchKompDTO>();
+
+		Set<DoubleInteger> keys = productBatchCompList.keySet();
+
 		for(DoubleInteger key : keys){
 			productbsc.add(productBatchCompList.get(key).copy());
 		}
@@ -47,17 +61,11 @@ public class MyProduktBatchKompDAO implements ProduktBatchKompDAO {
 	}
 
 	@Override
-	public List<ProduktBatchKompDTO> getProduktBatchKompList() throws DALException {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public void createProduktBatchKomp(ProduktBatchKompDTO produktbatchkomponent) throws DALException {
-		
+
 		if (productBatchCompList.putIfAbsent(new DoubleInteger(produktbatchkomponent.getPbId(), produktbatchkomponent.getRbId()), produktbatchkomponent.copy()) == null)
 			return;
-		
+
 		else
 			throw new DALException("Product Batch ID:"+produktbatchkomponent.getPbId()+" already exists !");
 
