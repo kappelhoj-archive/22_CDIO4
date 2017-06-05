0e361feb50260426255122fabd94edca4489ada9
Peter <peterxp@hotmail.fr>
2017-06-05 11:07:59 +0200

    ReceptDAO Data Layer implemented

---
+++ 22_CDIO4/src/dataAccessObjects/MyReceptDAO.java
@@ -0,0 +1,54 @@
+package dataAccessObjects;
+
+import java.util.ArrayList;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Set;
+
+import dataAccessObjects.interfaces.ReceptDAO;
+import dataTransferObjects.ReceptDTO;
+import exceptions.DALException;
+
+public class MyReceptDAO implements ReceptDAO {
+	
+	static Hashtable<Integer, ReceptDTO> recipeList = new Hashtable<Integer, ReceptDTO>();
+
+	@Override
+	public ReceptDTO getRecept(int receptId) throws DALException {
+		if(recipeList.get(receptId) != null)
+			return recipeList.get(receptId).copy();
+	
+		else
+			throw new DALException("Unknown Recipe ID: " + receptId);
+	}
+
+	@Override
+	public List<ReceptDTO> getReceptList() throws DALException {
+		List<ReceptDTO> recipes = new ArrayList<ReceptDTO>();
+
+		Set<Integer> keys = recipeList.keySet();
+		
+		for(Integer key : keys){
+			recipes.add(recipeList.get(key).copy());
+		}
+
+		return recipes;
+	}
+
+	@Override
+	public void createRecept(ReceptDTO recept) throws DALException {
+		if (recipeList.putIfAbsent(recept.getReceptId(), recept.copy()) == null)
+			return;
+		
+		else
+			throw new DALException("Recpipe ID:"+recept.getReceptId()+" already exists !");
+
+	}
+
+	@Override
+	public void updateRecept(ReceptDTO recept) throws DALException {
+		recipeList.replace(recept.getReceptId(), recept.copy());
+
+	}
+
+}
