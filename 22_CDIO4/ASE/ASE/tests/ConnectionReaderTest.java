fe368968749463a7014da7b3096cca78b384c789
Mads <mads@stege.eu>
2017-06-06 11:01:18 +0200

    Fully implemented Port test, works flawlessly.

--- 22_CDIO4/ASE/ASE/tests/ConnectionReaderTest.java
+++ 22_CDIO4/ASE/ASE/tests/ConnectionReaderTest.java
@@ -17,42 +17,63 @@ public class ConnectionReaderTest {
 
 	ConnectionReader connectionReader;
 	String fileLocation;
-	String[] IPArray;
 
 	@Before
 	public void setUp() throws Exception {
-		fileLocation = "ASE/ASE/tests/WeightTableTest.txt";
-		connectionReader = new ConnectionReader(fileLocation);
+
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		fileLocation = null;
+		connectionReader = null;
 	}
 
 	@Test
 	public void testIPChecker() {
+		fileLocation = "ASE/ASE/tests/WeightTableIPTest.txt";
+		connectionReader = new ConnectionReader(fileLocation);
+
 		try {
 			connectionReader.WeightReader();
 		} catch (FileNotFoundException e) {
 			System.out.println(e);
 		}
-		String[] expected = { "12.245.231.201", "91.23.110.232"};
-		
+		String[] expected = { "12.245.231.201", "91.23.110.232" };
+
 		ArrayList<String> actual;
 		actual = connectionReader.getAllIPAdresses();
-		
-		System.out.println(actual);
-		
-		for (int i = 0; i < expected.length; i++){
+
+		for (int i = 0; i < expected.length; i++) {
 			assertEquals(expected[i], actual.get(i));
 
 		}
-	}
-	
-	@Test
-	public void testPortChecker(){
+
 		
+	}
+
+	@Test
+	public void testPortChecker() {
+		fileLocation = "ASE/ASE/tests/WeightTablePortTest.txt";
+		connectionReader = new ConnectionReader(fileLocation);
+
+		try {
+			connectionReader.WeightReader();
+		} catch (FileNotFoundException e) {
+			System.out.println(e);
+		}
+
+		String[] expected = { "12319", "12315" };
+
+		ArrayList<String> actual;
+		actual = connectionReader.getAllPortNumbers();
+
+		for (int i = 0; i < expected.length; i++) {
+			assertEquals(expected[i], actual.get(i));
+
+		}
+		fileLocation = null;
+		connectionReader = null;
 	}
 
 }
