58f61792cfe68b2736e1dd81dc4dc60df0e69cce
Arvid Langsø <arvid.k.langsoe@live.dk>
2017-06-06 16:44:14 +0200

    Fixed some merging errors

--- 22_CDIO4/ASE/ASE/tests/MeasurementControllerTest.java
+++ 22_CDIO4/ASE/ASE/tests/MeasurementControllerTest.java
@@ -1,13 +1,14 @@
 package ASE.tests;
 import ASE.Controllers.MeasurementController;
-import dataTransferObjects.ProduktBatchKompDTO;
+import dataTransferObjects.ProductBatchCompDTO;
+
 
 public class MeasurementControllerTest {
 
 	public static void main(String[] args) {
 		MeasurementController test = new MeasurementController();
 		(new Thread(test)).start();
-		ProduktBatchKompDTO measurement = new ProduktBatchKompDTO(1, 1, 1.1, 1.1, 1);
+		ProductBatchCompDTO measurement = new ProductBatchCompDTO(1, 1, 1.1, 1.1, 1);
 
 		System.out.println(measurement);
 		test.enqueue(measurement);
