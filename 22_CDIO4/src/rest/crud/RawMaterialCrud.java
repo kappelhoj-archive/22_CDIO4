98554e649b3314ccd444795e75a42762ba61b776
Mikkel Holmbo Lund <math-lund@outlook.dk>
2017-06-06 11:01:47 +0200

    CRUD RawMaterial Done

--- 22_CDIO4/src/rest/crud/RawMaterialCrud.java
+++ 22_CDIO4/src/rest/crud/RawMaterialCrud.java
@@ -1,8 +1,12 @@
 package rest.crud;
 
+import java.util.List;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
 import controller.RawMaterialController;
@@ -47,8 +51,69 @@ public class RawMaterialCrud {
 		}
 	}
 	
-	public Arraylist<RawMaterial>
+	//TODO: Kig på HTTP ERROR
+	@Path("read_list")
+	@GET
+	@Produces(MediaType.APPLICATION_JSON)
+	public List<RaavareDTO> readRawMaterialList()
 	{
-		
+		try 
+		{
+			return controller.getRawMaterialList();
+		}
+		catch (DALException e)
+		{
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+			return null;
+		}
+	}
+	
+	@Path("update")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String updateRawMaterial(RaavareDTO rawMaterial)
+	{
+		try 
+		{
+			controller.updateRawMaterial(rawMaterial);
+			return "success";
+		}
+		catch (InputException e)
+		{
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+			return "Fejl: Det indtastede er ugyldigt..";
+		}
+		catch (DALException e)
+		{
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+			return "Fejl: Der skete en fejl i systemet.";
+		}
+	}
+	
+	//TODO: Kig på HTTP ERROR
+	@Path("read")
+	@POST
+	@Produces(MediaType.APPLICATION_JSON)
+	public RaavareDTO readRawMaterial(int id)
+	{
+		try
+		{
+		return controller.getRawMaterial(id);
+		}
+		catch (InputException e)
+		{
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+			return null;
+		}
+		catch (DALException e)
+		{
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+			return null;
+		}
 	}
 }
