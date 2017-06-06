749a09b1740ec4c23450548fb8c99f26391d8bcc
Mikkel Holmbo Lund <math-lund@outlook.dk>
2017-06-06 11:59:10 +0200

    RawMaterialBatchCRUD done

--- 22_CDIO4/src/rest/crud/RawMaterialBatchCRUD.java
+++ 22_CDIO4/src/rest/crud/RawMaterialBatchCRUD.java
@@ -1,5 +1,143 @@
 package rest.crud;
 
-public class RawMaterialBatchCRUD {
+import java.util.List;
 
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+import controller.RawMaterialBatchController;
+import controller.interfaces.IRawMaterialBatchController;
+import dataTransferObjects.RaavareBatchDTO;
+import exceptions.CollisionException;
+import exceptions.DALException;
+import exceptions.InputException;
+
+@Path("raw_material_batch")
+public class RawMaterialBatchCRUD {
+	
+	IRawMaterialBatchController controller = new RawMaterialBatchController();
+	
+	@Path("create")
+	@POST
+	@Consumes(MediaType.APPLICATION_JSON)
+	public String createRawMaterialBatch(RaavareBatchDTO rawMaterialBatch){
+		
+		try
+		{
+			controller.createRawMaterialBatch(rawMaterialBatch);
+			return "success";
+		}
+		catch (CollisionException e)
+		{
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+			return "Fejl: Der findes allerede en råvare med det indtastede id.";
+		}
+		catch (InputException e)
+		{
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+			return "Fejl: Det indtastede er ugyldigt..";
+		}
+		catch (DALException e)
+		{
+			e.printStackTrace();
+			System.out.println(e.getMessage());
+			return "Fejl: Der skete en fejl i systemet."; 
+		}
+	}
+	
+	
+	//TODO: Kig på HTTP ERROR
+		@Path("read_list")
+		@GET
+		@Produces(MediaType.APPLICATION_JSON)
+		public List<RaavareBatchDTO> readRawMaterialBatchList()
+		{
+			try 
+			{
+				return controller.getRawMaterialBatchList();
+			}
+			catch (DALException e)
+			{
+				e.printStackTrace();
+				System.out.println(e.getMessage());
+				return null;
+			}
+		}
+		
+		@Path("read_list_specific")
+		@POST
+		@Produces(MediaType.APPLICATION_JSON)
+		public List<RaavareBatchDTO> readRawMaterialBatchList(int raavareId)
+		{
+			try
+			{
+				return controller.getRawMaterialBatchList(raavareId);
+			}
+			catch (InputException e)
+			{
+				e.printStackTrace();
+				System.out.println(e.getMessage());
+				return null;
+			}
+			catch (DALException e)
+			{
+				e.printStackTrace();
+				System.out.println();
+				return null;
+			}
+		}
+		
+		@Path("update")
+		@POST
+		@Consumes(MediaType.APPLICATION_JSON)
+		public String updateRawMaterialBatch(RaavareBatchDTO rawMaterialBatch)
+		{
+			try 
+			{
+				controller.updateRawMaterialBatch(rawMaterialBatch);
+				return "success";
+			}
+			catch (InputException e)
+			{
+				e.printStackTrace();
+				System.out.println(e.getMessage());
+				return "Fejl: Det indtastede er ugyldigt..";
+			}
+			catch (DALException e)
+			{
+				e.printStackTrace();
+				System.out.println(e.getMessage());
+				return "Fejl: Der skete en fejl i systemet.";
+			}
+		}
+		
+		//TODO: Kig på HTTP ERROR
+		@Path("read")
+		@POST
+		@Produces(MediaType.APPLICATION_JSON)
+		public RaavareBatchDTO readRawMaterialBatch(int id)
+		{
+			try
+			{
+			return controller.getRawMaterialBatch(id);
+			}
+			catch (InputException e)
+			{
+				e.printStackTrace();
+				System.out.println(e.getMessage());
+				return null;
+			}
+			catch (DALException e)
+			{
+				e.printStackTrace();
+				System.out.println(e.getMessage());
+				return null;
+			}
+		}
 }
