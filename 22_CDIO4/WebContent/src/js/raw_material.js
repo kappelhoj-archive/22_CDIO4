8db44b78bc8b5317826dafb23b625afba0de552f
Mikkel Holmbo Lund <math-lund@outlook.dk>
2017-06-07 09:50:18 +0200

    create function created.

--- 22_CDIO4/WebContent/src/js/raw_material.js
+++ 22_CDIO4/WebContent/src/js/raw_material.js
@@ -2,11 +2,12 @@
  * 
  */
 
-$(document).ready(function(){
+$(document).ready(function()
+{
 	
 	/* Create raw material javascript */
 	
-	$(document).on("submit", "#raw_material_create_form", function(event){
+	$(document).on("submit", "#raw_material_create_form", function(event) {
 		event.preventDefault();
 		$.ajax({
 			url: "rest/raw_material/create",
@@ -16,13 +17,18 @@ $(document).ready(function(){
 			success: function(data) {
 				if(data == "success")
 				{
-					$.get("src/rawMaterialAdministration/raw_material_list.html", function(template) {
-			            $("#content").html(Mustache.render($(template).html(), getRawMaterialList())		            
-			        });
+					getRawMaterialList().done(function(data){
+						$.get("src/raw_material/raw_material_list.html", function(template) {
+							$("#content").html(Mustache.render($(template).html(),data))
+						});
+					})
+					.fail(function(data){
+						console.log(data);
+					});
 				}
 				else
 				{
-					alert("data");
+					alert(data);
 				}
 			},
 			error: function(data){
@@ -30,19 +36,14 @@ $(document).ready(function(){
 			}
 		});
 	});
+	
 });
 	
 
 function getRawMaterialList(){
-	var json;
-	$.ajax({
+	return $.ajax({
 		url: "rest/raw_material/read_list",
 		type: "GET",
-		contentType: "application/json",
-		success: function(data){
-			json = data;
-		}
+		contentType: "application/json"
 	});
-	return { getJson : function () {
-		if(json) return json;
 }
