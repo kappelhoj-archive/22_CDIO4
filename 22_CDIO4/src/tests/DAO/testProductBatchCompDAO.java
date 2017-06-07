46f8f0755df8162d0cd31694dd3bca0030125cb3
Peter <peterxp@hotmail.fr>
2017-06-07 14:38:34 +0200

    DAO Tests updated

--- 22_CDIO4/src/tests/DAO/testProductBatchCompDAO.java
+++ 22_CDIO4/src/tests/DAO/testProductBatchCompDAO.java
@@ -19,23 +19,108 @@ public class testProductBatchCompDAO {
 	@Before
 	public void setUp() throws Exception {
 		dao = new ProductBatchCompDAO();
+
 	}
 
 	@After
 	public void tearDown() throws Exception {
 		dao = null;
 	}
-	
+
+	@Test
+	public void startTest(){
+		try {
+			dao.createProductBatchComp(new ProductBatchCompDTO(9, 9, 1.1, 1.1, 11));
+		} catch (DALException e) {
+			fail(e.getMessage());
+			e.printStackTrace();
+		}
+	}
+
+	//Positive Test
 	@Test
 	public void testCreateAndGetProductBatchComp() {
 		try {
+			ProductBatchCompDTO expected = new ProductBatchCompDTO(1, 1, 1.1, 1.1, 11);
+
 			dao.createProductBatchComp(new ProductBatchCompDTO(1, 1, 1.1, 1.1, 11));
-			
-			
+
+			ProductBatchCompDTO actual = dao.getProductBatchComp(1, 1);
+
+			assertTrue(expected.equals(actual));
+
 		} catch (DALException e) {
 			e.printStackTrace();
 			fail("Error" + e.getMessage());
 		}
 	}
 
+	//Positive test
+	@Test
+	public void testUpdateProductBatchComp(){
+		try {
+			ProductBatchCompDTO expected = new ProductBatchCompDTO(1, 2, 1.2222, 8.1, 19);
+
+			dao.createProductBatchComp(new ProductBatchCompDTO(1, 2, 1.1, 1.1, 11));
+			dao.updateProductBatchComp(new ProductBatchCompDTO(1, 2, 1.2222, 8.1, 19));
+
+			ProductBatchCompDTO actual = dao.getProductBatchComp(1, 2);
+
+			assertTrue(expected.equals(actual));
+
+		} catch (DALException e) {
+			e.printStackTrace();
+			fail("Error" + e.getMessage());
+		}
+
+	}
+	
+	//Negative Test
+	@Test
+	public void testCreateProductBatchCompFail() {
+		
+		boolean expected = true;
+		boolean actual = false;
+		
+		try {
+			
+			dao.createProductBatchComp(new ProductBatchCompDTO(5, 5, 1.1, 1.1, 11));
+			dao.createProductBatchComp(new ProductBatchCompDTO(5, 5, 2.1, 3.1, 44));
+
+		} catch (DALException e) {
+			System.out.println(e);
+			actual = true;
+		}
+		
+		assertEquals(expected, actual);
+	}
+
+
+	//Abstract test
+	@Test
+	public void testGetProductBatchCompList(){
+		try {
+			System.out.println(dao.getProductBatchCompList());
+		} catch (DALException e) {
+			fail(e.getMessage());
+			e.printStackTrace();
+		}
+
+	}
+
+	//Abstract test
+	@Test
+	public void testGetProductBatchCompListByID(){
+		try {
+			System.out.println(dao.getProductBatchCompList(1));
+		} catch (DALException e) {
+			fail(e.getMessage());
+			e.printStackTrace();
+		}
+
+	}
+
+	
+	
 }
+//
