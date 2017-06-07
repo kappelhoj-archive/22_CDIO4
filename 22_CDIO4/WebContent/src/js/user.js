fe7a756ac5f5cf2d658cfb975f71451d5bff24c2
Jeppe <Jeppe@DESKTOP-HUK674P>
2017-06-07 10:19:17 +0200

    Updates to user templates and removed navigation.js from the project

--- 22_CDIO4/WebContent/src/js/user.js
+++ 22_CDIO4/WebContent/src/js/user.js
@@ -1,5 +1,50 @@
 $(document).ready(function() {
 	
+	/** 
+	 * User administration links
+	 * **/
+	
+	// Vis min bruger link
+	$(document).on("click", ".user_edit_link", function(event) {
+		event.preventDefault();
+		var userId = "1";
+		getUser(userId).done(function(data) {
+			$.get("src/html/user/user_edit.html", function(template) {
+	            $("#content").html(Mustache.render($(template).html(),data))		            
+	        });
+		})
+		.fail(function(x) {
+			console.log("Fejl!");
+		});
+	});
+	
+	// Vis alle brugere link
+	$(document).on("click", ".user_list_link", function(event) {
+		event.preventDefault();
+		
+		getUsers().done(function(data) {
+			$.get("src/html/user/user_list.html", function(template) {
+				$("#content").html(template);
+				$.each(data, function(i, data){
+					$.get("src/html/user/user_list_row.html", function(template) {
+			            $("#user_list .table tbody").append(Mustache.render($(template).html(),data))
+			        });
+				});
+	        });
+		})
+		.fail(function(x) {
+			console.log("Fejl!");
+		});
+	});
+	
+	// Vis alle brugere link
+	$(document).on("click", ".user_create_link", function(event) {
+		event.preventDefault();
+		
+		$.get("src/html/user/user_create.html", function(template) {
+            $("#content").html(template);		            
+        });
+	});
 });
 
 function getUser(userId) {
