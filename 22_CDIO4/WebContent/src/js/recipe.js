c235d4dac079cb80b503d590665c871a63ef5ce4
Mikkel Holmbo Lund <math-lund@outlook.dk>
2017-06-08 13:06:49 +0200

    recipe methods added.

--- 22_CDIO4/WebContent/src/js/recipe.js
+++ 22_CDIO4/WebContent/src/js/recipe.js
@@ -1,3 +1,103 @@
 /**
  * 
  */
+
+$(document).ready(function(){
+	
+/* ################################ Main page button functions ######################################### */
+
+/* Calls the showRecipeListPage() function when button with class="recipe_list_link" */
+$(document).on("click", ".recipe_list_link", function(event) {
+	event.preventDefault();
+	showRecipeListPage();
+});
+
+
+/* ################################ List page button functions ######################################### */
+
+/* Goes to recipe_create.html when button with id="create_recipe" is clicked */
+$(document).on("click", "#create_recipe", function(event){
+	event.preventDefault();
+	$.get("src/html/recipe/recipe_create.html", function(template) {
+		$("content").html(template);
+	});
+})
+
+/* Shows the recipe id and the recipe name and all of its components */
+$(document).on("click", ".recipe_comp_table_list_link", function(event) {
+	event.preventDefault();
+	var recipeId = $(this).parents("tr").children("td:first").text();
+	getRecipe(recipeId).done(function(data) {
+		$.get("src/html/recipe/recipe_edit", function(template) {
+			$(".input_fields").html(Mustache.render($(template).html(), data))
+		});
+	})
+	.fail(function(data){
+		console.log(data);
+	});
+	showRecipeCompsPage(recipeId);
+})
+
+})
+
+/* Functions */
+/* Creates a table of all the recipes in the system. And shows it in the section with id="content" */
+function showRecipeListPage() {
+	getRecipeList().done(function(data) {
+		$.get("src/html/recipe/recipe_list.html", function(template) {
+			$("#content").html(template);
+			$.each(data, function(i, data) {
+				$.get("src/html/recipe_list_row.html", function(template) {
+					$("#recipe_list .table tbody").append(Mustache.render($(template).html(), data))
+				});
+			});
+		});
+	})
+	.fail(function(data){
+		console.log("System fejl")
+		alert("System fejl")
+	})
+}
+
+/* Creates a table of all the recipe components in a specific recipe. */
+function showRecipeCompsPage(recipeId) {
+	getRecipeContent(recipeid).done(function(data) {
+		$.get("src/html/recipe/recipe_edit.html", function(template) {
+			$("#recipe_component_list").html(template);
+			$.each(data, function(i, data) {
+				$.get("src/html/recipe_comp_list_row.html", function(template) {
+					$("recipe_component_list .table tbody").append(Mustache.render($(template).html(), data))
+				});
+			});
+		});
+	});
+}
+
+
+function getRecipeList() {
+	return $.ajax({
+		url: "rest/recipe/read_list",
+		type: "GET",
+		contentType: "application/json"
+	});
+}
+
+
+
+function getRecipe(id){
+	return $.ajax({
+		url: "rest/recipe/read",
+		type: "POST",
+		data: id,
+		contentType: "application/json"
+	})
+}
+
+function getRecipeContent(recipeId){
+	return $.ajax({
+		url: "rest/recipe_component/read_list_specific",
+		type: "POST",
+		data: recipeId,
+		contentType: "application/json"
+	})
+}
