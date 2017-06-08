8616101da747a7bb423ac14cc834403435468782
Arvid Langsø <arvid.k.langsoe@live.dk>
2017-06-08 13:18:29 +0200

    Updated measurementDTO

--- 22_CDIO4/ASE/ASE/DTOs/MeasurementDTO.java
+++ 22_CDIO4/ASE/ASE/DTOs/MeasurementDTO.java
@@ -1,35 +1,40 @@
 package ASE.DTOs;
 
+import java.util.List;
+
+import dataTransferObjects.ProductBatchCompDTO;
+
 public class MeasurementDTO 
 	{
-		int pbId; 	  // produktbatchets id
-		int rbId;        // i omraadet 1-99999999
-		double tara;
-		double netto;
-		int oprId;					// operatoer-nummer
-
+		private int newStatus;
+		private int pbId;
+		private List<ProductBatchCompDTO> measurements;
 		
-		public MeasurementDTO(int pbId, int rbId, double tara, double netto, int oprId)
-		{
+		public MeasurementDTO(int newStatus, int pbId, List<ProductBatchCompDTO> measurements) {
+			super();
+			this.newStatus = newStatus;
 			this.pbId = pbId;
-			this.rbId = rbId;
-			this.tara = tara;
-			this.netto = netto;
-			this.oprId = oprId;
+			this.measurements = measurements;
 		}
 		
-		public int getPbId() { return pbId; }
-		public void setPbId(int pbId) { this.pbId = pbId; }
-		public int getRbId() { return rbId; }
-		public void setRbId(int rbId) { this.rbId = rbId; }
-		public double getTara() { return tara; }
-		public void setTara(double tara) { this.tara = tara; }
-		public double getNetto() { return netto; }
-		public void setNetto(double netto) { this.netto = netto; }
-		public int getOprId() { return oprId; }
-		public void setOprId(int oprId) { this.oprId = oprId; }
-		public String toString() { 
-			return pbId + "\t" + rbId +"\t" + tara +"\t" + netto + "\t" + oprId ; 
+		public int getNewStatus() {
+			return newStatus;
 		}
+		public void setNewStatus(int newStatus) {
+			this.newStatus = newStatus;
+		}
+		public int getPbId() {
+			return pbId;
+		}
+		public void setPbId(int pbId) {
+			this.pbId = pbId;
+		}
+		public List<ProductBatchCompDTO> getMeasurements() {
+			return measurements;
+		}
+		public void setMeasurements(List<ProductBatchCompDTO> measurements) {
+			this.measurements = measurements;
+		}
+		
 	}
 
