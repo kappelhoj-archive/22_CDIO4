633ba1f50aa9a538c4e111d808db0aaa250cb814
Arvid <Arvid@Arvid-Pc.clients.wireless.dtu.dk>
2017-06-09 14:47:20 +0200

    Test the weight

--- 22_CDIO4/ASE/ASE/tests/LiveWeightCommunicationTest.java
+++ 22_CDIO4/ASE/ASE/tests/LiveWeightCommunicationTest.java
@@ -1,19 +1,91 @@
 package ASE.tests;
 
+import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
 import ASE.Controllers.WeightCommunicator;
+import ASE.exceptions.InvalidReturnMessageException;
+import ASE.exceptions.LogOutException;
+import ASE.exceptions.ProtocolErrorException;
 
 public class LiveWeightCommunicationTest {
 	public static Socket client;
 	static WeightCommunicator weightCommunicator;
-	public static void main(String[] args) throws UnknownHostException, IOException {
-		client= new Socket("localhost", 8000);
-		weightCommunicator = new WeightCommunicator(client);
-		// TODO Auto-generated method stub
 
+	public static void main(String[] args) throws UnknownHostException, IOException {
+		client = new Socket("169.254.2.3", 8000);
+		weightCommunicator = new WeightCommunicator(client);
+
+		new DataOutputStream(client.getOutputStream()).writeBytes("K 3\n");
+		try {
+			System.out.println(weightCommunicator.waitForAnswer());
+		} catch (ProtocolErrorException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		weightCommunicator.restartWeightDisplay();
+		System.out.println("Tryk på knappe Simon!");
+
+		try {
+			Thread.sleep(2000);
+		} catch (InterruptedException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+
+		// try {
+		// System.out.println(weightCommunicator.getWeight());
+		// } catch (ProtocolErrorException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+		//
+		// try {
+		// System.out.println(weightCommunicator.receiveButtonPush());
+		//
+		// System.out.println(weightCommunicator.receiveButtonPush());
+		//
+		// System.out.println(weightCommunicator.receiveButtonPush());
+		//
+		// } catch (ProtocolErrorException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// } catch (LogOutException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+
+		// try {
+		// weightCommunicator.sendMessage("Hej Mads");
+		//
+		// } catch (InvalidReturnMessageException e) {
+		// try {
+		// System.out.println(weightCommunicator.receiveButtonPush());
+		// } catch (ProtocolErrorException | LogOutException e1) {
+		// // TODO Auto-generated catch block
+		// e1.printStackTrace();
+		// }
+		// }
+
+		// try {
+		// System.out.println(weightCommunicator.askForInformation("Skriv id"));
+		// } catch (InvalidReturnMessageException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+
+		// try {
+		// weightCommunicator.taraWeight();
+		// System.out.println(weightCommunicator.getWeight());
+		// } catch (ProtocolErrorException e) {
+		// // TODO Auto-generated catch block
+		// e.printStackTrace();
+		// }
+
+		client.close();
 	}
 
 }
