9f975ef5c3b4e760d46a9b323dbc6ee0b636dbd1
simonengquist <simon@engquist.dk>
2017-06-12 11:09:51 +0200

    fix int getWeight

--- 22_CDIO4/ASE/ASE/Controllers/WeightCommunicator.java
+++ 22_CDIO4/ASE/ASE/Controllers/WeightCommunicator.java
@@ -245,7 +245,7 @@ public class WeightCommunicator implements IWeightCommunicator {
 		sendProtocol(Protocol.Measurement, null);
 		answerReceived = waitForAnswer();
 		String[] splitAnswer=answerReceived.split(" ");
-		if (splitAnswer[0].contains("T")&&splitAnswer[1].contains("S"))
+		if (splitAnswer[0].contains("S")&&splitAnswer[1].contains("S"))
 		{
 			return Double.parseDouble(splitAnswer[splitAnswer.length-2]);
 		}
