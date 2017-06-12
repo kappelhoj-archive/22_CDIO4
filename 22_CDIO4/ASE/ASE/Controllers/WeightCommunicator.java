0425c7a4394337ac0d8b790c817069e2d668f910
simonengquist <simon@engquist.dk>
2017-06-12 11:14:38 +0200

    fin in cleanStream

--- 22_CDIO4/ASE/ASE/Controllers/WeightCommunicator.java
+++ 22_CDIO4/ASE/ASE/Controllers/WeightCommunicator.java
@@ -272,11 +272,7 @@ public class WeightCommunicator implements IWeightCommunicator {
 
 					return;
 				}
-				else{
-					previousMessageRecived=answerReceived;
-					throw new ProtocolErrorException(answerReceived);
-				}
-				//TODO: return of RM20 0
+
 			}
 		} catch (ProtocolErrorException e) {
 			// TODO Auto-generated catch block
