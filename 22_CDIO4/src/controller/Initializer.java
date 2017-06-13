e0d23bd00be04e250aa3cd23654c004bc1df1936
Peter <peterxp@hotmail.fr>
2017-06-13 09:51:19 +0200

    up

--- 22_CDIO4/src/controller/Initializer.java
+++ 22_CDIO4/src/controller/Initializer.java
@@ -41,7 +41,7 @@ public class Initializer implements ServletContextListener {
 	static ILoginController login = new LoginController(userDAO);
 	static IProductBatchCompController pbc = new ProductBatchCompController(productBatchCompDAO, productBatchDAO, rawMaterialBatchDAO);
 	static IProductBatchController pb = new ProductBatchController(productBatchDAO, recipeDAO);
-	static IRawMaterialBatchController rmb = new RawMaterialBatchController(rawMaterialBatchDAO);
+	static IRawMaterialBatchController rmb = new RawMaterialBatchController(rawMaterialBatchDAO, rawMaterialDAO);
 	static IRawMaterialController rm = new RawMaterialController(rawMaterialDAO);
 	static IRecipeCompController rc = new RecipeCompController(recipeCompDAO, recipeDAO, rawMaterialDAO);
 	static IRecipeController r = new RecipeController(recipeDAO);
