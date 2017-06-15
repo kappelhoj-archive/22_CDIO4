aac7c3f16949359d18e03b2049f5d8b94275a2ee
Peter <peterxp@hotmail.fr>
2017-06-15 09:18:50 +0200

    fix

--- 22_CDIO4/src/controller/UserController.java
+++ 22_CDIO4/src/controller/UserController.java
@@ -82,8 +82,6 @@ public class UserController implements IUserController{
 
 		if(user.getPassword() == null || user.getPassword()=="")
 			user.setPassword(dao.getUser(user.getId()).getPassword());
-
-		validation(user);
 		
 		dao.updateOperatoer(user);
 
